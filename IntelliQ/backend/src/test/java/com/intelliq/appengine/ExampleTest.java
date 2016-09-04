bbd15b5ba6615b6db68c76f41f44f5b9168408ec
Stephan Schultz <register@steppschuh.net>
2016-09-04 15:33:36 +0200

    Added backend test

---
+++ IntelliQ/backend/src/test/java/com/intelliq/appengine/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.intelliq.appengine;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void testAddition() {
+		assertEquals(5, 2 + 2);
+	}
+
+}
